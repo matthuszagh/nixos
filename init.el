59cfe4d08a82f02b0d1cec617255216f67d3db1f
matthuszagh <huszaghmatt@gmail.com>
2018-10-29 18:32:58 -0400

    Verilator can be used for syntax checking. Improved ycmd functionality over tramp.

--- init.el
+++ init.el
@@ -10,7 +10,7 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Specify the ycmd server command and path to the ycmd directory *inside *the;
 ; cloned ycmd directory
-(defvar my:ycmd-server-command '("python" "/home/matt/developer/software/ycmd/ycmd"))
+(defvar my:ycmd-server-command '("python3" "/home/matt/developer/software/ycmd/ycmd"))
 (defvar my:ycmd-extra-conf-whitelist '("~/.ycm_extra_conf.py"))
 (defvar my:ycmd-global-config "~/.ycm_extra_conf.py")
 
@@ -919,6 +919,7 @@ Please set my:ycmd-server-command appropriately in ~/.emacs.d/init.el.\n"
         (require 'ycmd-eldoc)
         (add-hook 'ycmd-mode-hook 'ycmd-eldoc-setup)
         )
+      (setq ycmd-request-message-level -1)
       )
   )
 
@@ -971,6 +972,8 @@ Please set my:ycmd-server-command appropriately in ~/.emacs.d/init.el.\n"
   ;; https://github.com/abingham/emacs-ycmd
   (when (not (display-graphic-p))
     (setq flycheck-indication-mode nil))
+  ;; Verilog Verilator static analyzer
+  (setq-default flycheck-verilog-verilator-executable "/usr/local/bin/verilator_bin")
   )
 (use-package flycheck-pyflakes
   :ensure t
@@ -1017,7 +1020,7 @@ Please set my:ycmd-server-command appropriately in ~/.emacs.d/init.el.\n"
  '(git-gutter:update-interval 5)
  '(package-selected-packages
    (quote
-    (ov hide-lines flymake-cppcheck py-autopep8 flycheck-clang-analyzer djvu flycheck-plantuml plantuml-mode etable el-get deadgrep 0xc ac-slime slime sx google-this helm-system-packages symon company-restclient restclient sage-shell-mode auctex-latexmk nov nasm-mode x86-lookup buffer-move evil pdf-tools qt-pro-mode auto-complete-exuberant-ctags markdown-mode yasnippet-snippets elpy realgud beacon wgrep use-package zzz-to-char yasnippet yapfify yaml-mode writegood-mode window-numbering which-key web-mode vlf test-simple swiper-helm string-inflection sourcerer-theme ripgrep rainbow-delimiters pyvenv powerline origami multiple-cursors modern-cpp-font-lock magit-gerrit loc-changes load-relative json-mode hungry-delete highlight-indentation google-c-style git-gutter flyspell-correct-ivy flycheck-ycmd flycheck-pyflakes elscreen-multi-term ein edit-server cuda-mode counsel-etags company-ycmd company-jedi cmake-font-lock clang-format bind-key autopair auto-package-update auctex 0blayout)))
+    (flycheck-verilator flycheck-verilog-verilator ov hide-lines flymake-cppcheck py-autopep8 flycheck-clang-analyzer djvu flycheck-plantuml plantuml-mode etable el-get deadgrep 0xc ac-slime slime sx google-this helm-system-packages symon company-restclient restclient sage-shell-mode auctex-latexmk nov nasm-mode x86-lookup buffer-move evil pdf-tools qt-pro-mode auto-complete-exuberant-ctags markdown-mode yasnippet-snippets elpy realgud beacon wgrep use-package zzz-to-char yasnippet yapfify yaml-mode writegood-mode window-numbering which-key web-mode vlf test-simple swiper-helm string-inflection sourcerer-theme ripgrep rainbow-delimiters pyvenv powerline origami multiple-cursors modern-cpp-font-lock magit-gerrit loc-changes load-relative json-mode hungry-delete highlight-indentation google-c-style git-gutter flyspell-correct-ivy flycheck-ycmd flycheck-pyflakes elscreen-multi-term ein edit-server cuda-mode counsel-etags company-ycmd company-jedi cmake-font-lock clang-format bind-key autopair auto-package-update auctex 0blayout)))
  '(plantuml-jar-path "/usr/share/plantuml/plantuml.jar"))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;

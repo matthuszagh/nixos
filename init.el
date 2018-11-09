9cafbc3cdc89f6799aa52c163b5adf52056adf73
matthuszagh <huszaghmatt@gmail.com>
2018-11-09 11:46:16 -0500

    Add documentation for Emacs setup. Highlight current source line when debugging.

--- init.el
+++ init.el
@@ -850,6 +850,26 @@ rotate entire document."
 ;; GDB configuration
 (setq gdb-many-windows t)
 
+(defvar gud-overlay
+  (let* ((ov (make-overlay (point-min) (point-min))))
+    (overlay-put ov 'face 'secondary-selection)
+    ov)
+  "Overlay variable for GUD highlighting.")
+(defadvice gud-display-line (after my-gud-highlight act)
+  "Highlight current line."
+  (let* ((ov gud-overlay)
+         (bf (gud-find-file true-file)))
+    (with-current-buffer bf
+      (move-overlay ov (line-beginning-position) (line-beginning-position 2)
+                    ;;(move-overlay ov (line-beginning-position) (line-end-position)
+                    (current-buffer)))))
+(defun gud-kill-buffer ()
+  (if (derived-mode-p 'gud-mode)
+      (delete-overlay gud-overlay)))
+(add-hook 'kill-buffer-hook 'gud-kill-buffer)
+
+
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Modern C++ code highlighting
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;

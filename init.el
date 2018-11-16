9a937fbdea9fe96fa1e6b0b8216c9f18e677c22a
matthuszagh <huszaghmatt@gmail.com>
2018-11-15 21:28:27 -0500

    Suppress unnecessary warnings at startup.

--- init.el
+++ init.el
@@ -4,6 +4,9 @@
 
 ;;; Code:
 
+;; Suppress free variable warnings
+(setq byte-compile-warnings '(not free-vars obsolete))
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Configuration/Customization:
 ;; Defines global variables that are later used to customize and set
@@ -202,6 +205,7 @@ entire document."
 rotate entire document."
   (interactive "P")
   (pdf-view--rotate :counterclockwise (not arg)))
+
 
 ;; workaround for pdf-tools not reopening to last-viewed page of the pdf:
 ;; https://github.com/politza/pdf-tools/issues/18#issuecomment-269515117
@@ -498,11 +502,11 @@ rotate entire document."
   (save-restriction
     ;; Suppress the warning when you setq an undefined variable.
     (if (>= emacs-major-version 23)
-        (setq byte-compile-warnings '(not free-vars obsolete))
+        (setq byte-compile-warnings '(not free-vars obsolete redefine))
       (setq byte-compile-warnings
             '(unresolved
               callargs
-              redefine
+              ;; redefine
               obsolete
               noruntime
               cl-warnings
@@ -877,7 +881,6 @@ rotate entire document."
 (setq gdb-many-windows t)
 
 ;; Suppress warnings with reference to free variable.
-(defvar gdb-source-window)
 ;; Keep source line centered on screen
 (defadvice gud-display-line (after gud-display-line-centered activate)
   "Center the line in the window"
@@ -1142,12 +1145,13 @@ Please set my:ycmd-server-command appropriately in ~/.emacs.d/init.el.\n"
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(Man-notify-method 'pushy)
+ '(Man-notify-method (quote pushy))
  '(flymake-cppcheck-enable "all")
  '(git-gutter:update-interval 5)
  '(jit-lock-context-time 0.1)
  '(package-selected-packages
-   '(yasnippet-snippets company-ycmd ycmd flycheck-plantuml flycheck-clang-analyzer flycheck-pyflakes flycheck debbugs exec-path-from-shell libmpdee ivy-mpdel mpdel auto-dim-other-buffers flycheck-verilator flycheck-verilog-verilator ov hide-lines flymake-cppcheck py-autopep8 djvu plantuml-mode etable el-get deadgrep 0xc ac-slime slime sx google-this helm-system-packages symon company-restclient restclient sage-shell-mode auctex-latexmk nov nasm-mode x86-lookup buffer-move evil pdf-tools qt-pro-mode auto-complete-exuberant-ctags markdown-mode elpy realgud beacon wgrep use-package zzz-to-char yasnippet yapfify yaml-mode writegood-mode window-numbering which-key web-mode vlf test-simple swiper-helm string-inflection sourcerer-theme ripgrep rainbow-delimiters pyvenv powerline origami multiple-cursors modern-cpp-font-lock magit-gerrit loc-changes load-relative json-mode hungry-delete highlight-indentation google-c-style git-gutter flyspell-correct-ivy elscreen-multi-term ein edit-server cuda-mode counsel-etags company-jedi cmake-font-lock clang-format bind-key autopair auto-package-update auctex 0blayout))
+   (quote
+    (yasnippet-snippets company-ycmd ycmd flycheck-plantuml flycheck-clang-analyzer flycheck-pyflakes flycheck debbugs exec-path-from-shell libmpdee ivy-mpdel mpdel auto-dim-other-buffers flycheck-verilator flycheck-verilog-verilator ov hide-lines flymake-cppcheck py-autopep8 djvu plantuml-mode etable el-get deadgrep 0xc ac-slime slime sx google-this helm-system-packages symon company-restclient restclient sage-shell-mode auctex-latexmk nov nasm-mode x86-lookup buffer-move evil pdf-tools qt-pro-mode auto-complete-exuberant-ctags markdown-mode elpy realgud beacon wgrep use-package zzz-to-char yasnippet yapfify yaml-mode writegood-mode window-numbering which-key web-mode vlf test-simple swiper-helm string-inflection sourcerer-theme ripgrep rainbow-delimiters pyvenv powerline origami multiple-cursors modern-cpp-font-lock magit-gerrit loc-changes load-relative json-mode hungry-delete highlight-indentation google-c-style git-gutter flyspell-correct-ivy elscreen-multi-term ein edit-server cuda-mode counsel-etags company-jedi cmake-font-lock clang-format bind-key autopair auto-package-update auctex 0blayout)))
  '(plantuml-jar-path "/usr/share/plantuml/plantuml.jar")
  '(symon-mode nil))
 
@@ -1670,6 +1674,7 @@ Please set my:ycmd-server-command appropriately in ~/.emacs.d/init.el.\n"
  '(company-tooltip-common-selection ((t (:foreground "#93a1a1" :underline t))))
  '(company-tooltip-mouse ((t (:background "DodgerBlue4" :foreground "CadetBlue1"))))
  '(company-tooltip-selection ((t (:background "DodgerBlue4" :foreground "CadetBlue1"))))
+ '(cursor ((t (:background "dim gray"))))
  '(header-line ((t (:background "#003366"))))
  '(ivy-current-match ((((class color) (background light)) (:background "#555555")) (((class color) (background dark)) (:background "DodgerBlue4"))))
  '(ivy-minibuffer-match-face-1 ((((class color) (background light)) (:background "#555555")) (((class color) (background dark)) (:background "#555555"))))

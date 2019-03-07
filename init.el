d9fd450daaf6ba1c600351550a6e9e088a6d1ed8
Matt Huszagh <huszaghmatt@gmail.com>
2019-03-06 18:57:12 -0800

    Remove unused magithub

--- init.el
+++ init.el
@@ -1282,43 +1282,6 @@ Please set my:ycmd-server-command appropriately in ~/.emacs.d/init.el.\n"
 (use-package magit-gerrit
   :after magit)
 
-;; (use-package magithub
-;;   :after magit
-;;   :config
-;;   (magithub-feature-autoinject t)
-;;   ;; limit filters
-;;   (with-eval-after-load 'magithub
-;;     (require 'parse-time)
-
-;;     (defmacro magithub--time-number-of-days-since-string (iso8601)
-;;       `(time-to-number-of-days
-;;         (time-since
-;;          (parse-iso8601-time-string
-;; 	  (concat ,iso8601 "+00:00")))))
-
-;;     (defun issue-filter-to-days (days type)
-;;       `(lambda (issue)
-;;          (let ((created_at (magithub--time-number-of-days-since-string
-;; 			    (alist-get 'created_at issue)))
-;; 	       (updated_at (magithub--time-number-of-days-since-string
-;; 			    (alist-get 'updated_at issue))))
-;; 	   (or (< created_at ,days) (< updated_at ,days)))))
-
-;;     (defun magithub-filter-maybe (&optional limit)
-;;       "Add filters to magithub only if number of issues is greter than LIMIT."
-;;       (let ((max-issues (length (ignore-errors (magithub-issues))))
-;; 	    (max-pull-requests (length (ignore-errors (magithub-pull-requests))))
-;; 	    (limit (or limit 15)))
-;;         (when (> max-issues limit)
-;; 	  (add-to-list (make-local-variable 'magithub-issue-issue-filter-functions)
-;; 		       (issue-filter-to-days limit "issues")))
-;;         (when (> max-pull-requests limit)
-;; 	  (add-to-list (make-local-variable 'magithub-issue-pull-request-filter-functions)
-;; 		       (issue-filter-to-days limit "pull-requests")))))
-
-;;     (add-to-list 'magit-status-mode-hook #'magithub-filter-maybe))
-;;   (setq magithub-clone-default-directory "~/developer"))
-
 ;; Displays small signals to the left of line numbers in git repos to indicate diffs since the last
 ;; commit. A purple '=' means modified, a red '-' means deleted and a green '+' means added.
 (use-package git-gutter

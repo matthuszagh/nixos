dac9f9d1e68e56bc86edfec48b0e6f0f0e3f3bf0
Matt Huszagh <huszaghmatt@gmail.com>
2019-04-08 09:14:37 -0700

    Revert appearance to a version before customizations
    The default version actually looks better. The
    change was made by removing code in custom.el.

--- custom.el
+++ custom.el
@@ -3,29 +3,6 @@
 ;;; Commentary:
 
 ;;; Code:
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "#222222" :foreground "#c2c2b0" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :width normal :foundry "PfEd" :family "SF Mono"))))
- '(auto-dim-other-buffers-face ((t (:background "#111"))))
- '(company-preview ((t (:background "#073642" :foreground "#2aa198"))))
- '(company-preview-common ((t (:foreground "#93a1a1" :underline t))))
- '(company-scrollbar-bg ((t (:background "#073642" :foreground "#2aa198"))))
- '(company-scrollbar-fg ((t (:foreground "#002b36" :background "#839496"))))
- '(company-template-field ((t (:background "#7B6000" :foreground "#073642"))))
- '(company-tooltip ((t (:background "black" :foreground "DeepSkyBlue1"))))
- '(company-tooltip-annotation ((t (:foreground "#93a1a1" :background "#073642"))))
- '(company-tooltip-common ((t (:foreground "#93a1a1" :underline t))))
- '(company-tooltip-common-selection ((t (:foreground "#93a1a1" :underline t))))
- '(company-tooltip-mouse ((t (:background "DodgerBlue4" :foreground "CadetBlue1"))))
- '(company-tooltip-selection ((t (:background "DodgerBlue4" :foreground "CadetBlue1"))))
- '(header-line ((t (:background "#003366"))))
- '(mode-line ((t (:background "DodgerBlue4" :distant-foreground "#5c5d56" :weight normal))))
- '(mode-line-buffer-id ((t (:foreground "#8686ae"))))
- '(term-color-red ((t (:background "#aa4450" :foreground "#aa4450"))))
- '(which-func ((t (:foreground "#8fb28f")))))
 
 (provide 'custom)
 ;;; custom.el ends here

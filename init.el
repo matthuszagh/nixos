5d5fbc11deaf149e9b32a5ca13bd186c405199b9
Matt Huszagh <huszaghmatt@gmail.com>
2019-04-10 16:02:22 -0700

    Add function to check all modes for a key binding
    This is useful when you're considering assigning a
    new keybinding so as to avoid conflicts. The code
    for this comes from here:
    https://emacs.stackexchange.com/a/15114/20317

--- init.el
+++ init.el
@@ -323,6 +323,34 @@ amount of spaces."
 ;; smart tab behavior - indent or complete
 (setq tab-always-indent 'complete)
 
+;; Utility to find keybinding in all modes.
+(with-eval-after-load "s"
+  (defun list-known-bindings (key)
+    (interactive "kList known bindings for key: ")
+    (with-current-buffer (get-buffer-create "*known bindings*")
+      (erase-buffer)
+      (mapatoms (lambda (sym)
+                  (when (or (eq sym 'global-map)
+                            (and (boundp sym)
+                                 (symbol-value sym)
+                                 (s-ends-with-p "-mode-map" (symbol-name sym))
+                                 (keymapp (symbol-value sym))))
+                    (let ((binding (lookup-key (symbol-value sym) key t)))
+                      (when (and binding
+                                 (not (numberp binding)))
+                        (insert (format "%-40s `%s'\n"
+                                        (format "`%s'" sym)
+                                        (if (keymapp binding)
+                                            "KEYMAP"
+                                          binding))))))))
+      (sort-lines nil (point-min) (point-max))
+      (goto-char (point-min))
+      (insert
+       (format "Known bindings for key: %s\n\n" (key-description key))
+       (format "%-40s %s" "Map" "Binding\n")
+       (s-repeat 40 "-") " " (s-repeat 30 "-") "\n")
+      (display-buffer (current-buffer)))))
+
 ;; Install use-package with straight.
 (straight-use-package 'use-package)
 ;; default :straight t

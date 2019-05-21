be0c081bb42a236a9faa821cbaaecc786682eb45
Matt Huszagh <huszaghmatt@gmail.com>
2019-05-22 00:34:01 +0900

    Small changes to custom and documentation

--- custom.el
+++ custom.el
@@ -11,5 +11,13 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(git-gutter:update-interval 5)
+ '(custom-safe-themes
+   (quote
+    ("c44056a7a2426e1229749e82508099ba427dde80290da16a409d5d692718aa11" "c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" default)))
  '(helm-source-names-using-follow (quote ("Recentf"))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )

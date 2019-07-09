9ec50121f95e93680b4c9bb92b20d281b074f9b8
Matt Huszagh <huszaghmatt@gmail.com>
2019-07-09 15:07:58 -0700

    Switch out of exwm windows with C-SPC
    Set the initial state of all exwm buffers to
    insert state and whitelist C-SPC. This allows
    using this as a way to switch out of an exwm
    buffer. SPC doesn't work because we often need to
    type spaces into exwm buffers.

--- layers/exwm-layer.el
+++ layers/exwm-layer.el
@@ -21,6 +21,13 @@
                                       "--output DP-0 --left-of eDP-1-1 "
                                       "--output DP-2 --right-of eDP-1-1"))))
     (setq exwm-randr-workspace-monitor-plist '(0 "eDP-1-1" 1 "DP-0" 2 "DP-2"))
-    (exwm-randr-enable)))
+    (exwm-randr-enable))
+
+  :postsetup
+  (:layer modal-interaction
+          ;; stop exwm from catching SPC leader key.
+          ;; the double whitespace is intentional
+          (add-to-list 'exwm-input-prefix-keys ?\C-\  )
+          (evil-set-initial-state 'exwm-mode 'insert)))
 
 ;;; exwm-layer.el ends here

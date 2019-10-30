865730a7d778d48caa868df49bf12da2a031e3a7
Matt Huszagh <huszaghmatt@gmail.com>
2019-10-30 14:02:45 -0700

    org: change code appearance

--- layers/default-theme-layer.el
+++ layers/default-theme-layer.el
@@ -18,8 +18,18 @@
   :postsetup
   (:layer org
    (set-face-foreground 'org-block (face-foreground 'default))
+
+   ;; set inline code appearance
+   (set-face-background 'org-code "unspecified")
+   (set-face-foreground 'org-code "#8686ae")
+   (set-face-foreground 'org-link "#86aed5")
+
    ;;(face-remap-add-relative 'org-block `(:foreground ,(face-foreground 'default)))
-   ))
+   )
+
+  (:layer default-mode-line
+   (set-face-foreground 'sml/global "#c2c2b0")
+   (set-face-foreground 'sml/filename "gold")))
 
 
 ;;; default-theme-layer.el ends here

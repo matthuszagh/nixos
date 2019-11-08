09cd7ab3ed77dad2f07e57814b504e28dd61519f
Matt Huszagh <huszaghmatt@gmail.com>
2019-11-07 18:15:56 -0800

    rmsbolt: add convenience keybindings

--- layers/rmsbolt-layer.el
+++ layers/rmsbolt-layer.el
@@ -17,13 +17,26 @@
              (lambda ()
                (setq-local rmsbolt-command "clang -O0 -target riscv64"))))
 
+  (:layer (c modal)
+   (localleader :keymaps 'c-mode-base-map
+     "C" 'rmsbolt-compile))
+
   (:layer elisp
    (add-hook 'emacs-lisp-mode-hook 'rmsbolt-mode))
+  (:layer (elisp modal)
+   (localleader :keymaps 'elisp-mode-map
+     "C" 'rmsbolt-compile))
 
   (:layer python
    (add-hook 'python-mode-hook 'rmsbolt-mode))
+  (:layer (python modal)
+   (localleader :keymaps 'python-mode-map
+     "C" 'rmsbolt-compile))
 
   (:layer rust
-   (add-hook 'rust-mode-hook 'rmsbolt-mode)))
+   (add-hook 'rust-mode-hook 'rmsbolt-mode))
+  (:layer (rust modal)
+   (localleader :keymaps 'rust-mode-map
+     "C" 'rmsbolt-compile)))
 
 ;;; rmsbolt-layer.el ends here

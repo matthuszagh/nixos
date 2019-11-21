067dd0f37d8bbd4f60ec00babf180cf3d981d71f
Matt Huszagh <huszaghmatt@gmail.com>
2019-11-20 21:10:10 -0800

    snippet-layer: add hooks for org, verilog and elisp modes

--- layers/snippet-layer.el
+++ layers/snippet-layer.el
@@ -8,17 +8,24 @@
   :setup
   (use-package yasnippet
     :config
-    (add-to-list 'yas-snippet-dirs (concat user-emacs-directory "snippets/")))
-
-  (use-package yasnippet-snippets
-    :config
+    (add-to-list 'yas-snippet-dirs (concat user-emacs-directory "snippets/"))
     (yas-reload-all))
+
+  ;; (use-package yasnippet-snippets
+  ;;   :config
+  ;;   (yas-reload-all))
 
   :postsetup
   (:layer TeX
    (add-hook 'latex-mode-hook #'yas-minor-mode))
 
   (:layer org
-   (add-hook 'org-mode-hook #'yas-minor-mode)))
+   (add-hook 'org-mode-hook #'yas-minor-mode))
+
+  (:layer verilog
+   (add-hook 'verilog-mode-hook #'yas-minor-mode))
+
+  (:layer elisp
+   (add-hook 'emacs-lisp-mode-hook #'yas-minor-mode)))
 
 ;;; snippet-layer.el ends here

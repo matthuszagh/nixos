963e5095ddd0a20389769342008ebb2305bc018f
Matt Huszagh <huszaghmatt@gmail.com>
2019-12-07 23:30:01 -0800

    org: remove whitespace from latex fragments
    * layers/org-layer.el (org): override `org-create-formula-image' with
    advice. This should be merged upstream.

--- layers/org-layer.el
+++ layers/org-layer.el
@@ -23,6 +23,99 @@
                    (setq-local fill-column 70)))
      (org-babel-after-execute . org-display-inline-images))
     :config
+    (defun mh//override-org-create-formula-image
+        (string tofile options buffer &optional processing-type)
+      "Create an image from LaTeX source using external processes.
+
+The LaTeX STRING is saved to a temporary LaTeX file, then
+converted to an image file by process PROCESSING-TYPE defined in
+`org-preview-latex-process-alist'.  A nil value defaults to
+`org-preview-latex-default-process'.
+
+The generated image file is eventually moved to TOFILE.
+
+The OPTIONS argument controls the size, foreground color and
+background color of the generated image.
+
+When BUFFER non-nil, this function is used for LaTeX previewing.
+Otherwise, it is used to deal with LaTeX snippets showed in
+a HTML file."
+      (let* ((processing-type (or processing-type
+			          org-preview-latex-default-process))
+	     (processing-info
+	      (cdr (assq processing-type org-preview-latex-process-alist)))
+	     (programs (plist-get processing-info :programs))
+	     (error-message (or (plist-get processing-info :message) ""))
+	     (image-input-type (plist-get processing-info :image-input-type))
+	     (image-output-type (plist-get processing-info :image-output-type))
+	     (post-clean (or (plist-get processing-info :post-clean)
+			     '(".dvi" ".xdv" ".pdf" ".tex" ".aux" ".log"
+			       ".svg" ".png" ".jpg" ".jpeg" ".out")))
+	     (latex-header
+	      (or (plist-get processing-info :latex-header)
+	          (org-latex-make-preamble
+	           (org-export-get-environment (org-export-get-backend 'latex))
+	           org-format-latex-header
+	           'snippet)))
+	     (latex-compiler (plist-get processing-info :latex-compiler))
+	     (image-converter (plist-get processing-info :image-converter))
+	     (tmpdir temporary-file-directory)
+	     (texfilebase (make-temp-name
+		           (expand-file-name "orgtex" tmpdir)))
+	     (texfile (concat texfilebase ".tex"))
+	     (image-size-adjust (or (plist-get processing-info :image-size-adjust)
+				    '(1.0 . 1.0)))
+	     (scale (* (if buffer (car image-size-adjust) (cdr image-size-adjust))
+		       (or (plist-get options (if buffer :scale :html-scale)) 1.0)))
+	     (dpi (* scale (if buffer (org--get-display-dpi) 140.0)))
+	     (fg (or (plist-get options (if buffer :foreground :html-foreground))
+		     "Black"))
+	     (bg (or (plist-get options (if buffer :background :html-background))
+		     "Transparent"))
+	     (log-buf (get-buffer-create "*Org Preview LaTeX Output*"))
+	     (resize-mini-windows nil)) ;Fix Emacs flicker when creating image.
+        (dolist (program programs)
+          (org-check-external-command program error-message))
+        (if (eq fg 'default)
+	    (setq fg (org-latex-color :foreground))
+          (setq fg (org-latex-color-format fg)))
+        (if (eq bg 'default)
+	    (setq bg (org-latex-color :background))
+          (setq bg (org-latex-color-format
+		    (if (string= bg "Transparent") "white" bg))))
+        ;; remove tex \par at end of snippet to avoid trailing
+        ;; whitespace
+        (if (string= (substring string -1 nil) "\n")
+            (aset string (- (length string) 1) ?%)
+          (setq string (concat string "%")))
+        (with-temp-file texfile
+          (insert latex-header)
+          (insert "\n\\begin{document}\n"
+	          "\\definecolor{fg}{rgb}{" fg "}%\n"
+	          "\\definecolor{bg}{rgb}{" bg "}%\n"
+	          "\n\\pagecolor{bg}%\n"
+	          "\n{\\color{fg}\n"
+	          string
+	          "\n}\n"
+	          "\n\\end{document}\n"))
+        (let* ((err-msg (format "Please adjust `%s' part of \
+`org-preview-latex-process-alist'."
+			        processing-type))
+	       (image-input-file
+	        (org-compile-file
+	         texfile latex-compiler image-input-type err-msg log-buf))
+	       (image-output-file
+	        (org-compile-file
+	         image-input-file image-converter image-output-type err-msg log-buf
+	         `((?D . ,(shell-quote-argument (format "%s" dpi)))
+	           (?S . ,(shell-quote-argument (format "%s" (/ dpi 140.0))))))))
+          (copy-file image-output-file tofile 'replace)
+          (dolist (e post-clean)
+	    (when (file-exists-p (concat texfilebase e))
+	      (delete-file (concat texfilebase e))))
+          image-output-file)))
+
+    (advice-add 'org-create-formula-image :override #'mh//override-org-create-formula-image)
     (general-define-key
      :keymaps 'org-mode-map
      "TAB" 'org-cycle

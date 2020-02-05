4430ac52abc2a61c40bd924cde2cdbed796c606f
Matt Huszagh <huszaghmatt@gmail.com>
2020-02-04 17:02:06 -0800

    documentation: add info history keybinding

--- layers/documentation-layer.el
+++ layers/documentation-layer.el
@@ -15,6 +15,15 @@
 
   (use-package info-colors
     :config
-    (add-hook 'Info-selection-hook 'info-colors-fontify-node)))
+    (add-hook 'Info-selection-hook 'info-colors-fontify-node))
+
+  (use-package info)
+
+  :postsetup
+  (:layer modal
+   (general-define-key
+    :states 'normal
+    :keymaps 'Info-mode-map
+    "H" 'Info-history)))
 
 ;;; documentation-layer.el ends here

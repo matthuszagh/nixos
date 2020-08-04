b27f56b22364623a35ddbf125c59f1621a93d564
Matt Huszagh <huszaghmatt@gmail.com>
2020-08-04 07:55:35 -0700

    power module: change tlp extraConfig to settings

--- modules/power.nix
+++ modules/power.nix
@@ -8,11 +8,11 @@
     upower.enable = true;
     tlp = {
       enable = true;
-      extraConfig = ''
-        USB_BLACKLIST_PHONE=1
-        CPU_HWP_ON_AC=performance
-        CPU_HWP_ON_BAT=power
-      '';
+      settings = {
+        USB_BLACKLIST_PHONE = 1;
+        CPU_HWP_ON_AC = "performance";
+        CPU_HWP_ON_BAT = "power";
+      };
     };
   };
 }

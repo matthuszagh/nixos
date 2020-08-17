7808803259a79d694946877d496b7bf8eb08fb44
Matt Huszagh <huszaghmatt@gmail.com>
2020-08-17 15:11:52 -0700

    locate profile: init

--- .old/configuration.nix
+++ .old/configuration.nix
@@ -24,7 +24,6 @@ in
 {
   imports = [
     # ============================ system ============================
-    (modules-path + "/locate.nix")
     (modules-path + "/sound.nix")
     (modules-path + "/power.nix")
     (modules-path + "/ssh.nix")

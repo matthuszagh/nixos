faa9977c6e533776723c8b5cc40383392d31b582
Matt Huszagh <huszaghmatt@gmail.com>
2020-08-24 08:43:13 -0700

    base-layer: add screenshot svg function

--- layers/base-layer.el
+++ layers/base-layer.el
@@ -213,6 +213,17 @@ hasn't changed. This clears the image cache to prevent this."
 
   (defun mh/insert-current-date ()
     (interactive)
-    (insert (shell-command-to-string "echo -n $(date +%d/%m/%Y)"))))
+    (insert (shell-command-to-string "echo -n $(date +%d/%m/%Y)")))
+
+  (defun mh/screenshot-svg ()
+    "Save a screenshot of the current frame as an SVG image.
+Saves to a temp file and puts the filename in the kill ring."
+    (interactive)
+    (let* ((filename (make-temp-file "Emacs" nil ".svg"))
+           (data (x-export-frames nil 'svg)))
+      (with-temp-file filename
+        (insert data))
+      (kill-new filename)
+      (message filename))))
 
 ;;; base-layer.el ends here

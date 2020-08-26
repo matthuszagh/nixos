fa8645712315f587703340e607c91f814a345004
Matt Huszagh <huszaghmatt@gmail.com>
2020-08-25 19:34:53 -0700

    org-roam-layer: update modified header property on save

--- users/profiles/emacs/emacs/layers/org-roam-layer.el
+++ users/profiles/emacs/emacs/layers/org-roam-layer.el
@@ -18,8 +18,19 @@
           `(("d" "default" plain #'org-roam-capture--get-point
              "%?"
              :file-name "${title}"
-             :head ,(concat "#+title: ${title}\n"
-                           "#+roam_alias: ")
-             :unnarrowed t)))))
+             :head ,(concat "#+TITLE: ${title}\n"
+                            "#+ROAM_ALIAS: \n"
+                            "#+CREATED: %(format-time-string \"[%Y-%m-%d %a %H:%M]\")"
+                            "#+MODIFIED: %(format-time-string \"[%Y-%m-%d %a %H:%M]\")")
+             :unnarrowed t)))
+    (defun mh//org-update-last-modified ()
+      (save-excursion
+        (goto-char 0)
+        (search-forward "MODIFIED: ")
+        (delete-region (point) (line-end-position))
+        (let ((now (format-time-string "[%Y-%m-%d %a %H:%M]")))
+          (insert now))))
+    (add-hook 'org-mode-hook (lambda ()
+                               (add-hook 'before-save-hook 'mh//org-update-last-modified nil t)))))
 
 ;;; org-roam-layer.el ends here

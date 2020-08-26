3861c7e15573d53bcf7042d685cd22a81b8fbbf3
Matt Huszagh <huszaghmatt@gmail.com>
2020-08-25 19:35:10 -0700

    org-roam-bibtex-layer: extend capture for noter

--- users/profiles/emacs/emacs/layers/org-roam-bibtex-layer.el
+++ users/profiles/emacs/emacs/layers/org-roam-bibtex-layer.el
@@ -20,7 +20,17 @@
           '(("r" "ref" plain #'org-roam-capture--get-point
              ""
              :file-name "refs/${citekey}"
-             :head "#+TITLE: ${title}\n#+ROAM_KEY: ${ref}\n"
+             :head "#+TITLE: ${title}\n#+ROAM_KEY: ${ref}
+
+- tags ::
+
+* outline
+:PROPERTIES:
+:Custom_ID: ${citekey}
+:URL: ${url}
+:AUTHOR: ${author-or-editor}
+:NOTER_DOCUMENT: %(orb-process-file-field \"${citekey}\")
+:END:"
              :unnarrowed t)))))
 
 ;;; org-roam-bibtex-layer.el ends here

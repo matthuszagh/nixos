85befef3adcde1cb508f6d790858715f5ce83b46
Matt Huszagh <huszaghmatt@gmail.com>
2020-08-27 22:12:42 -0700

    naysayer-theme-layer: customize helm colors

--- users/profiles/emacs/emacs/layers/naysayer-theme-layer.el
+++ users/profiles/emacs/emacs/layers/naysayer-theme-layer.el
@@ -31,6 +31,15 @@
    ;; org todo keyword faces
    (set-face-foreground 'org-todo "#d4d4d4")
    (setq org-todo-keyword-faces '(("TODO" . (:foreground "Pink"
-                                             :weight bold))))))
+                                             :weight bold)))))
+  (:layer org-ref
+   (set-face-foreground 'org-ref-ref-face "#ffaa00"))
+
+  (:layer helm
+   (set-face-attribute 'helm-ff-directory nil :foreground "RoyalBlue3" :background "unspecified")
+   (set-face-attribute 'helm-selection nil :foreground "unspecified" :background "blue")
+   (let ((fcolor "unspecified"))
+     (set-face-attribute 'helm-ff-file-extension nil :foreground fcolor)
+     (set-face-attribute 'helm-ff-file nil :foreground fcolor))))
 
 ;;; naysayer-theme-layer.el ends here

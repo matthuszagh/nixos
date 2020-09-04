c9100044f4664971aa538db6c3bc12e1365d0886
Matt Huszagh <huszaghmatt@gmail.com>
2020-09-03 21:05:10 -0700

    replace quantikz with qcircuit
    Even though qcircuit doesn't use tikz and quantikz does, quantikz
    appears to have bugs and the developer is only available via email.

--- users/profiles/tex/default.nix
+++ users/profiles/tex/default.nix
@@ -24,7 +24,7 @@
         pythontex
         asymptote
         animate
-        quantikz xargs xstring environ trimspaces tikz-cd
+        qcircuit xypic
         luatex85
         pygmentex
         pdftex
@@ -45,5 +45,6 @@
   home.file."texmf/tex/latex/commonstuff/math.cls".source = ./math.cls;
   home.file."texmf/tex/latex/commonstuff/common_local.sty".source = ./common_local.sty;
   home.file."texmf/tex/latex/commonstuff/math_local.sty".source = ./math_local.sty;
+  home.file."texmf/tex/latex/commonstuff/math_op_local.tex".source = ./math_op_local.tex;
   home.file."texmf/tex/latex/commonstuff/tikz_local.sty".source = ./tikz_local.sty;
 }

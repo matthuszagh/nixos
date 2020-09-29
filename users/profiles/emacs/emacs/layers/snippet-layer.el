94e1b363460de01ee9ae377f90663385edfd25f8
Matt Huszagh <huszaghmatt@gmail.com>
2020-09-29 10:11:33 -0700

    snippet-layer: use new math snippets

--- users/profiles/emacs/emacs/layers/snippet-layer.el
+++ users/profiles/emacs/emacs/layers/snippet-layer.el
@@ -1,4 +1,4 @@
-;;; snippet-layer.el --- Summary -*- lexical-binding: t; -*-
+;;; snippet-layer.el --- Snippet Layer -*- lexical-binding: t; -*-
 
 ;;; Commentary:
 
@@ -7,7 +7,9 @@
 (layer-def snippet
   :presetup
   (:layer straight
-   (straight-use-package 'yasnippet))
+   (straight-use-package '(yasnippet :type git :host github :repo "Zetagon/yasnippet"))
+   (straight-use-package '(auto-activating-snippets :type git :host github :repo "ymarco/auto-activating-snippets"))
+   (straight-use-package '(LaTeX-auto-activating-snippets :type git :host github :repo "tecosaur/LaTeX-auto-activating-snippets")))
 
   :setup
   (use-package yasnippet
@@ -22,6 +24,23 @@
         (let ((yas-buffer-local-condition ''(require-snippet-condition . auto)))
           (yas-expand))))
     (add-hook 'post-command-hook #'mh/yas-try-expanding-auto-snippets))
+
+  (use-package auto-activating-snippets
+    :hook (LaTeX-mode . auto-activating-snippets-mode)
+    :hook (org-mode . auto-activating-snippets-mode)
+    :config
+    (add-hook 'org-src-mode-hook
+              (lambda ()
+                (if (eq major-mode 'latex-mode)
+                    (auto-activating-snippets-mode)))))
+
+  (use-package latex-auto-activating-snippets
+    :after latex
+    :config
+    (apply #'aas-set-snippets 'org-mode laas-basic-snippets)
+    (apply #'aas-set-snippets 'org-mode laas-subscript-snippets)
+    (apply #'aas-set-snippets 'org-mode laas-frac-snippet)
+    (apply #'aas-set-snippets 'org-mode laas-accent-snippets))
 
   ;; (use-package yasnippet-snippets
   ;;   :config

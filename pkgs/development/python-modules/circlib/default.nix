b191ecc559ef0d2669469cad17f7674c5b2a96e2
Matt Huszagh <huszaghmatt@gmail.com>
2020-10-05 10:41:38 -0700

    update circlib to fetch repo from github

--- pkgs/development/python-modules/circlib/default.nix
+++ pkgs/development/python-modules/circlib/default.nix
@@ -1,4 +1,5 @@
 { stdenv
+, fetchFromGitHub
 , buildPythonPackage
 , numpy
 , matplotlib
@@ -9,7 +10,13 @@
 buildPythonPackage rec {
   pname = "circlib";
   version = "0.1.0";
-  src = /home/matt/src/circlib;
+
+  src = fetchFromGitHub {
+    owner = "matthuszagh";
+    repo = "circlib";
+    rev = "5ce6852b9c47754c0ee52136cbd6dce5a6b14fc6";
+    sha256 = "0zba1wip951qfxa5rry1yrssgka17sp0cszpd1l993a50ms5ba2j";
+  };
 
   propagatedBuildInputs = [
     numpy
--- pkgs/development/python-modules/skidl/default.nix
+++ pkgs/development/python-modules/skidl/default.nix
@@ -7,23 +7,19 @@
 , enum34
 , pyspice
 , graphviz
-, wxPython_4_0
 , pillow
-, pytest
-, setuptools
 , cffi
-, kicad
 }:
 
 buildPythonPackage rec {
   pname = "skidl";
-  version = "unstable-2020-05-30";
+  version = "unstable-2020-09-15";
 
   src = fetchFromGitHub {
     owner = "xesscorp";
     repo = "skidl";
-    rev = "07544b50c554874d4d99158deba1a987a00898cf";
-    sha256 = "0fr27vnhwfy974jdvjzp5ygywin82i743nfczwin6naaxfc9lqam";
+    rev = "551bdb92a50c0894b0802c0a89b4cb62a5b4038f";
+    sha256 = "1g65cyxpkqshgsggav2q3f76rbj5pzh7sacyhmhzvfz4zfarkcxk";
   };
 
   propagatedBuildInputs = [
@@ -33,24 +29,13 @@ buildPythonPackage rec {
     enum34
     pyspice
     graphviz
-    wxPython_4_0
     pillow
-    pytest
-    setuptools
     cffi
-    kicad
   ];
 
-  # TODO
-  # KICAD_SYMBOL_DIR = "${kicad.out}/share/kicad/library";
+  # Checks require availability of the kicad symbol libraries.
   doCheck = false;
   pythonImportsCheck = [ "skidl" ];
-
-  # checkPhase = ''
-  #   runHook preCheck;
-  #   ${python3Packages.pytest.out}/bin/pytest tests
-  #   runHook postCheck;
-  # '';
 
   meta = with stdenv.lib; {
     description = "SKiDL is a module that extends Python with the ability to design electronic circuits";

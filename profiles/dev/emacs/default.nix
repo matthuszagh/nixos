c724a18ff06f75337576c1e2f42545a868719f38
Matt Huszagh <huszaghmatt@gmail.com>
2020-10-06 06:47:46 -0700

    disable emacs service

--- profiles/dev/emacs/default.nix
+++ profiles/dev/emacs/default.nix
@@ -1,20 +1,26 @@
 { pkgs
 , ...
 }:
-
 let
-  emacsEnv = (pkgs.emacsPackagesFor (pkgs.emacsNative)).emacsWithPackages
-  (epkgs: (with epkgs.elpaPackages; [
-  ]) ++ (with epkgs.melpaPackages; [
-    vterm
-    lsp-python-ms  # microsoft python language server
-  ]) ++ (with epkgs.orgPackages; [
-  ]) ++ (with epkgs; [
-    pdf-tools
-  ]));
+  emacsEnv = (pkgs.emacsPackagesFor (pkgs.emacsNative)).emacsWithPackages (epkgs:
+    (with epkgs.elpaPackages; [
+    ]) ++ (with epkgs.melpaPackages; [
+      vterm
+      lsp-python-ms # microsoft python language server
+    ]) ++ (with epkgs.orgPackages; [
+    ]) ++ (with epkgs; [
+      pdf-tools
+    ]));
 in
 {
-  environment.systemPackages = with pkgs; [
+  environment.systemPackages = [
     emacsEnv
   ];
+
+  # services.emacs = {
+  #   enable = true;
+  #   package = emacsEnv;
+  #   defaultEditor = true;
+  #   install = true;
+  # };
 }

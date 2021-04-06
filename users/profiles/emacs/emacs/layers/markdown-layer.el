c52fa0b0fdb1e1aae0b1932e43611bf5405437c9
Matt Huszagh <huszaghmatt@gmail.com>
2021-04-06 13:38:41 -0700

    markdown-layer: modify evil navigation for visual-line-mode

--- users/profiles/emacs/emacs/layers/markdown-layer.el
+++ users/profiles/emacs/emacs/layers/markdown-layer.el
@@ -13,6 +13,17 @@
     :mode (("README\\.md\\'" . gfm-mode)
            ("\\.md\\'" . markdown-mode)
            ("\\.markdown\\'" . markdown-mode))
-    :init (setq markdown-command "multimarkdown")))
+    :init (setq markdown-command "multimarkdown"))
+
+  :postsetup
+  (:layer modal
+   (general-define-key
+    :keymaps 'markdown-mode-map
+    :states '(normal motion)
+    "j" 'evil-next-visual-line
+    "k" 'evil-previous-visual-line
+    "$" 'evil-end-of-line-or-visual-line
+    "0" 'evil-beginning-of-visual-line
+    "V" 'evil-visual-screen-line)))
 
 ;;; markdown-layer.el ends here

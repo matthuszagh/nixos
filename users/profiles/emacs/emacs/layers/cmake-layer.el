74e65143d6e577eacd8d3e705deb1f3036de6256
Matt Huszagh <huszaghmatt@gmail.com>
2021-04-07 08:48:28 -0700

    cmake-layer: install with straight

--- users/profiles/emacs/emacs/layers/cmake-layer.el
+++ users/profiles/emacs/emacs/layers/cmake-layer.el
@@ -3,21 +3,26 @@
 ;;; Code:
 
 (layer-def cmake
+  :presetup
+  (:layer straight
+   (straight-use-package 'cmake-mode)
+   (straight-use-package 'cmake-font-lock))
+
   :setup
   (use-package cmake-mode
     :mode ("CMakeLists.txt" ".cmake")
     :hook (cmake-mode . (lambda ()
-        (set (make-local-variable 'company-backends)
-          (list
-            (cons 'company-cmake company-backends)))))
+                          (set (make-local-variable 'company-backends)
+                               (list
+                                (cons 'company-cmake company-backends)))))
     :config
     (use-package cmake-font-lock
       :defer t
       :commands (cmake-font-lock-activate)
       :hook (cmake-mode . (lambda ()
-          (cmake-font-lock-activate)
-          (font-lock-add-keywords
-            nil '(("\\<\\(FIXME\\|TODO\\|BUG\\|DONE\\)"
-                1 font-lock-warning-face t))))))))
+                            (cmake-font-lock-activate)
+                            (font-lock-add-keywords
+                             nil '(("\\<\\(FIXME\\|TODO\\|BUG\\|DONE\\)"
+                                    1 font-lock-warning-face t))))))))
 
 ;;; cmake-layer.el ends here

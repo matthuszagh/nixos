8fdc4b515224b964ef7568019be5daf4651a0fc3
Matt Huszagh <huszaghmatt@gmail.com>
2021-04-17 15:45:32 -0700

    line-layer: remove visual-fill-column

--- users/profiles/emacs/emacs/layers/line-layer.el
+++ users/profiles/emacs/emacs/layers/line-layer.el
@@ -6,23 +6,12 @@
 ;;; Code:
 
 (layer-def line
-  :presetup
-  (:layer straight
-   (straight-use-package 'visual-fill-column))
-
   :setup
   (add-hook 'text-mode-hook
             (lambda ()
               ;; TeX modes are derived from text-mode, but we prefer
               ;; to treat TeX-mode like a programming mode.
               (if (not TeX-mode-p)
-                  (visual-line-mode))))
-
-  (use-package visual-fill-column
-    :hook
-    ((visual-line-mode . visual-fill-column-mode))
-    :config
-    ;; (add-hook 'markdown-mode-hook #'visual-fill-column-mode)
-    (setq visual-fill-column-width 70)))
+                  (visual-line-mode)))))
 
 ;;; line-layer.el ends here

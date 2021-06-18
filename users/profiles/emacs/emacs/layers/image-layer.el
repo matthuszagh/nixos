9eb6f20984c0b8bd5436dedaa6c06f158f0b98b2
Matt Huszagh <huszaghmatt@gmail.com>
2021-06-18 05:52:29 -0700

    image-layer: add function to replace black with currentColor in SVGs

--- users/profiles/emacs/emacs/layers/image-layer.el
+++ users/profiles/emacs/emacs/layers/image-layer.el
@@ -15,6 +15,13 @@
     "i" 'image-increase-size
     "o" 'image-decrease-size
     "l" 'image-next-frame
-    "h" 'image-previous-frame)))
+    "h" 'image-previous-frame))
+
+  :func
+  (defun mh/svg-replace-black-with-current-color ()
+    "Replace black strokes and fills with currentColor in current SVG document."
+    (interactive)
+    (replace-string "stroke:#000000" "stroke:currentColor")
+    (replace-string "fill:#000000" "fill:currentColor")))
 
 ;;; image-layer.el ends here

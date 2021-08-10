caabb1b27bdcff5ce7b0bef2a0617514cd7dc27b
Matt Huszagh <huszaghmatt@gmail.com>
2021-08-09 22:55:26 -0700

    disable completions in org-mode buffers

--- users/profiles/emacs/emacs/layers/completions-layer.el
+++ users/profiles/emacs/emacs/layers/completions-layer.el
@@ -36,11 +36,21 @@
    (general-def company-active-map
      "<tab>" 'company-complete-common))
 
+  ;; TODO this is temporary since completions do not currently work
+  ;; with org-roam and they make editing slower. However, this should
+  ;; be fixed at some point in the future at which point they should
+  ;; be reenabled. See the commented code below.
   (:layer org-roam
-   (setq org-roam-completion-everywhere t)
-   (add-hook 'org-mode-hook 'company-mode)
+   ;; disable company mode in org mode buffers
    (add-hook 'org-mode-hook
              (lambda ()
-               (setq-local company-backends '(company-capf))))))
+               (company-mode 0))))
+  ;; (:layer org-roam
+  ;;  (setq org-roam-completion-everywhere t)
+  ;;  ;; (add-hook 'org-mode-hook 'company-mode)
+  ;;  (add-hook 'org-mode-hook
+  ;;            (lambda ()
+  ;;              (setq-local company-backends '(company-capf)))))
+  )
 
 ;;; completions-layer.el ends here

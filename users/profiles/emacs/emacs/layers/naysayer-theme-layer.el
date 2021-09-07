667dac749b945577e8f6c63a4b0ea548c42f10e6
Matt Huszagh <huszaghmatt@gmail.com>
2021-09-06 20:05:20 -0700

    improve some naysayer theme color settings

--- users/profiles/emacs/emacs/layers/naysayer-theme-layer.el
+++ users/profiles/emacs/emacs/layers/naysayer-theme-layer.el
@@ -17,7 +17,9 @@
     (load-theme 'naysayer t)
     (setq mh-cursor-color (face-background 'cursor))
     (setq mh-foreground-color (face-foreground 'default))
-    (setq mh-background-color (face-background 'default)))
+    (setq mh-background-color (face-background 'default))
+    (set-face-attribute 'highlight nil :background "dark slate blue")
+    (set-face-attribute 'region nil :background "dark slate blue"))
 
   :postsetup
   (:layer org
@@ -28,6 +30,16 @@
    (set-face-foreground 'org-code (face-foreground 'default))
    (set-face-foreground 'org-link "#86aed5")
 
+   ;; org-level-n are inherited from outline-n.
+   (set-face-foreground 'outline-1 "white")
+   (set-face-foreground 'outline-2 "#44b34")
+   (set-face-foreground 'outline-3 "light sky blue")
+   (set-face-foreground 'outline-4 "khaki")
+   (set-face-foreground 'outline-5 "light grey")
+   (set-face-foreground 'outline-6 "#ffaa00")
+   ;; light pink
+   ;; light purple
+
    ;; org todo keyword faces
    (set-face-foreground 'org-todo "#d4d4d4")
    (setq org-todo-keyword-faces '(("TODO" . (:foreground "Pink"
@@ -37,9 +49,14 @@
 
   (:layer helm
    (set-face-attribute 'helm-ff-directory nil :foreground "white" :background (face-background 'default))
-   (set-face-attribute 'helm-selection nil :background "blue")
+   (set-face-attribute 'helm-selection nil :background "dark slate blue")
    (let ((fcolor (face-foreground 'default)))
      (set-face-attribute 'helm-ff-file-extension nil :foreground fcolor)
-     (set-face-attribute 'helm-ff-file nil :foreground fcolor))))
+     (set-face-attribute 'helm-ff-file nil :foreground fcolor)))
+
+  (:layer librarian
+   (set-face-foreground 'librarian-face-title "white")))
+
+
 
 ;;; naysayer-theme-layer.el ends here

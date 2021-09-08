aeccff71415d798a32c2eedfe217046288430eae
Matt Huszagh <huszaghmatt@gmail.com>
2021-09-07 20:34:33 -0700

    fix undo-tree

--- users/profiles/emacs/emacs/layers/undoing-layer.el
+++ users/profiles/emacs/emacs/layers/undoing-layer.el
@@ -5,21 +5,25 @@
 ;;; Code:
 
 (layer-def undoing
+  :presetup
+  (:layer straight
+   (straight-use-package 'undo-tree))
+
   :setup
   ;; Increase undo limits
   (setq undo-limit 16000000)
   (setq undo-strong-limit 24000000)
 
-  ;; (use-package undo-tree
-  ;;   :hook
-  ;;   (undo-tree-visualizer-mode . (lambda ()
-  ;;                                  (auto-save-mode 1)))
-  ;;   :config
-  ;;   (global-undo-tree-mode)
-  ;;   (setq undo-tree-visualizer-diff t)
-  ;;   ;; Save undo information persistently (i.e. across sessions)
-  ;;   (setq undo-tree-auto-save-history t)
-  ;;   (setq undo-tree-visualizer-timestamps t))
+  (use-package undo-tree
+    :hook
+    (undo-tree-visualizer-mode . (lambda ()
+                                   (auto-save-mode 1)))
+    :config
+    (global-undo-tree-mode)
+    (setq undo-tree-visualizer-diff t)
+    ;; Save undo information persistently (i.e. across sessions)
+    (setq undo-tree-auto-save-history t)
+    (setq undo-tree-visualizer-timestamps t))
 
   :postsetup
   (:layer keybinding-management

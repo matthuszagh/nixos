5d7f0d34eeb14a524bb4a43963d194ff64772fe8
Matt Huszagh <huszaghmatt@gmail.com>
2021-10-02 21:02:14 -0700

    allow sftp for ssh
    This is true by default, but it's good to be explicit in case the
    default changes.

--- profiles/networking/ssh.nix
+++ profiles/networking/ssh.nix
@@ -8,6 +8,7 @@
     # connecting host's public key be available in known_hosts.
     passwordAuthentication = false;
     permitRootLogin = "no";
+    allowSFTP = true;
   };
 
   # The SSH agent remembers private keys so that you don't have to

9731d2fe919fdc30302bf77e61ad68f62b1da702
Matt Huszagh <huszaghmatt@gmail.com>
2021-10-18 08:28:51 -0700

    emacs: use nix to manage vterm and pdf-tools

--- profiles/dev/emacs/default.nix
+++ profiles/dev/emacs/default.nix
@@ -3,12 +3,8 @@
 }:
 let
   emacsEnv = (pkgs.emacsPackagesFor (pkgs.emacsNative)).emacsWithPackages (epkgs:
-    (with epkgs.elpaPackages; [
-    ]) ++ (with epkgs.melpaPackages; [
+    (with epkgs.melpaPackages; [
       vterm
-      lsp-python-ms # microsoft python language server
-    ]) ++ (with epkgs.orgPackages; [
-    ]) ++ (with epkgs; [
       pdf-tools
     ]));
 in

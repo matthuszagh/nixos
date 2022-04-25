91ee2abb0744e967ab4c01058a486b6fc53b2f2b
Matt Huszagh <huszaghmatt@gmail.com>
2022-04-25 07:35:44 -0700

    add default inkscape template

--- users/profiles/inkscape/default.nix
+++ users/profiles/inkscape/default.nix
@@ -4,4 +4,5 @@
 
 {
   home.file.".config/inkscape/symbols/electrical-engineering.svg".source = ./symbols/electrical-engineering.svg;
+  home.file.".config/inkscape/templates/default.svg".source = ./templates/default.svg;
 }

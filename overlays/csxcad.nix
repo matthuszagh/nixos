0829a2ae7ea72dc9044bf19132c511ee207ad541
Matt Huszagh <huszaghmatt@gmail.com>
2022-05-18 17:38:03 -0700

    update csxcad overlay

--- overlays/csxcad.nix
+++ overlays/csxcad.nix
@@ -2,11 +2,13 @@ final: prev:
 
 {
   csxcad = (prev.csxcad.override { }).overrideAttrs (oldAttrs: {
+    version = "2022.05.19";
+
     src = prev.fetchFromGitHub {
       owner = "thliebig";
       repo = "CSXCAD";
-      rev = "ea97d3992fa5c4f9a3b5c2c5639449504ccf7b90";
-      sha256 = "sha256-O4+kZW2ol62GoIY0B4IlUOOP4awZM3n7+/+x8tB0nVg=";
+      rev = "cd9decb4d9cebe3c8bf115e2c0ee73f730f22da1";
+      sha256 = "sha256-NquEspnDg4pJc65M+uSFaW3MFe+fvwOZR7Wdu2FVBJg=";
     };
   });
 }
